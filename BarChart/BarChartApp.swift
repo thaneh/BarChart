5f24be514e45f0f99abbc770af2f28af4738d1cd
Thane Heninger <tsheninger@control4.com>
2020-10-13 22:21:40 -0600

    Initial Commit

---
+++ BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Thane Heninger on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarChart/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BarChart
+//
+//  Created by Thane Heninger on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SwiftUIView(gate: "open")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
